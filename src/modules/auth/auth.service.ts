d5b70bc1edeb70ba3504bdcf11743327e36260ca
Muhammadrizo Daminboev <m701rizo@gmail.com>
2026-02-16 13:59:12 +0500

    updated auth

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -34,8 +34,7 @@ export class AuthService {
                 firstName: user.firstName,
                 lastName: user.lastName,
                 role: user.role,
-                createdAt: user.createdAt,
-                updateAt: user.updatedAt,
+                phoneNumer: user.phoneNumer,
             }
         };
     }
